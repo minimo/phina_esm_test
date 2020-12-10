221587fe8d31b6f17bdb162f83075d3d07505216
minimo <minimo.mobile@gmail.com>
2020-12-10 20:49:11 +0900

    とりあえず画面表示まで

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import { phina, CanvasApp } from "phina.js"
-import {} from "/src/scenes/MainScene"
+import { MainScene } from "/src/scenes/MainScene"
 
 /* main */
 phina.main(()=> {
   const app = new CanvasApp();
   const mainScene = new MainScene();
   app.replaceScene(mainScene);
+  app.run();
 });
--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -2,8 +2,10 @@ import { DisplayScene, TriangleShape } from "phina.js"
 
 /* scene定義 */
 export class MainScene extends DisplayScene {
-  constructor(options) {
-    super(options);
+  constructor() {
+    super({
+      backgroundColor: 'black',
+    });
     this.player = new TriangleShape()
       .addChildTo(this)
       .setPosition(this.width/2, this.height/2)
